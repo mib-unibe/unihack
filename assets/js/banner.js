09b468a615cef45d957a7f7454c64cad01e20f21
Yannik Daellenbach <yannik.daellenbach@protonmail.com>
2019-01-12 20:20:58 +0100

    Add OBJLoader

--- assets/js/banner.js
+++ assets/js/banner.js
@@ -1,1 +1,22 @@
 console.log("Hello world");
+
+let loader = new THREE.OBJLoader();
+
+// load a resource
+loader.load(
+  // resource URL
+  "LED.obj",
+  // called when resource is loaded
+  function(object) {
+    console.log(object);
+    //scene.add(object);
+  },
+  // called when loading is in progresses
+  function(xhr) {
+    //console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  // called when loading has errors
+  function(error) {
+    console.error("An error happened");
+  }
+);
