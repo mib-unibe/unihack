e765431d2088eff78b03a4c7501e8580b7f31642
Yannik Daellenbach <yannik.daellenbach@protonmail.com>
2019-01-25 23:16:18 +0100

    Fix in and out with this workaround

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,48 @@
 (() => {
-  // Menu animation
-  let inTimeline = anime.timeline({
+  let currentAnimation = null;
+
+  const outTimeline = anime.timeline({
     autoplay: false,
-    begin: function(anim) {
-      document.getElementById("Overlay2").style.display = "none";
-    },
+    complete: anim => {
+      if (anim.direction === "reverse") {
+        document.getElementById("Overlay").style.display = "block";
+        anim.direction = "normal";
+      } else {
+        document.getElementById("Overlay").style.display = "none";
+        currentAnimation = inTimeline;
+      }
+    }
+  });
+
+  outTimeline.add({
+    targets: [".MenuOverlay-Item", ".LanguageSwitcher"],
+    opacity: [1, 0],
+    translateY: [
+      { value: 0, easing: "easeOutElastic(1, .5)" },
+      { value: -2, easing: "easeOutElastic(1, .5)" }
+    ],
+    delay: anime.stagger(100, { direction: "reverse", ease: "inOutElastic" }),
+    easing: "linear"
+  });
+
+  outTimeline.add({
+    targets: "#Overlay",
+    opacity: [1, 0],
+    duration: 200,
+    easing: "linear"
+  });
+
+  // Menu animation
+  const inTimeline = anime.timeline({
+    autoplay: false,
     complete: function(anim) {
       if (anim.direction === "reverse") {
+        anim.direction = "normal";
         document.getElementById("Overlay").style.display = "none";
       } else {
-        document.getElementById("Overlay2").style.display = "block";
+        console.log("Set other animation");
+        currentAnimation = outTimeline;
       }
-
-      anim.reverse();
     }
   });
 
@@ -29,17 +59,22 @@
     .add({
       targets: [".MenuOverlay-Item", ".LanguageSwitcher"],
       opacity: [0, 1],
-      translateY: [-20, 0],
-      delay: anime.stagger(100),
-      ease: "inOutElastic"
+      translateY: [
+        { value: -2, easing: "easeOutElastic(1, .5)" },
+        { value: 0, easing: "easeOutElastic(1, .5)" }
+      ],
+      delay: anime.stagger(100, { ease: "inOutElastic" }),
+      easing: "linear"
     });
+
+  currentAnimation = inTimeline;
 
   document.getElementById("hamburger-toggle").addEventListener(
     "click",
     function(e) {
-      inTimeline.play();
-      if (inTimeline.began) {
-        inTimeline.reverse();
+      currentAnimation.play();
+      if (currentAnimation.began) {
+        currentAnimation.reverse();
       }
     },
     false
