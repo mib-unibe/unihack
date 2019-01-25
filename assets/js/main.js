9316221da481f0f2b4a7e28de5b9d1dcf7922f82
Yannik Daellenbach <yannik.daellenbach@protonmail.com>
2019-01-25 22:26:59 +0100

    Wrap into function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,47 @@
-// Menu animation
-let inTimeline = anime.timeline({
-  autoplay: false,
-  begin: function(anim) {
-    document.getElementById("Overlay2").style.display = "none";
-  },
-  complete: function(anim) {
-    if (anim.direction === "reverse") {
-      document.getElementById("Overlay").style.display = "none";
-    } else {
-      document.getElementById("Overlay2").style.display = "block";
-    }
-    
-    anim.reverse();
-  }
-});
-
-inTimeline
-  .add({
-    targets: "#Overlay",
-    opacity: [0, 1],
-    duration: 200,
-    begin: () => {
-      document.getElementById("Overlay").style.display = "block";
+(() => {
+  // Menu animation
+  let inTimeline = anime.timeline({
+    autoplay: false,
+    begin: function(anim) {
+      document.getElementById("Overlay2").style.display = "none";
     },
-    easing: "linear"
-  })
-  .add({
-    targets: [".MenuOverlay-Item", ".LanguageSwitcher"],
-    opacity: [0, 1],
-    translateY: [-20, 0],
-    delay: anime.stagger(100),
-    ease: "inOutElastic"
+    complete: function(anim) {
+      if (anim.direction === "reverse") {
+        document.getElementById("Overlay").style.display = "none";
+      } else {
+        document.getElementById("Overlay2").style.display = "block";
+      }
+
+      anim.reverse();
+    }
   });
 
-document.getElementById("hamburger-toggle").addEventListener(
-  "click",
-  function(e) {
-    inTimeline.play();
-    if (inTimeline.began) {
-      inTimeline.reverse();
-    }
-  },
-  false
-);
+  inTimeline
+    .add({
+      targets: "#Overlay",
+      opacity: [0, 1],
+      duration: 200,
+      begin: () => {
+        document.getElementById("Overlay").style.display = "block";
+      },
+      easing: "linear"
+    })
+    .add({
+      targets: [".MenuOverlay-Item", ".LanguageSwitcher"],
+      opacity: [0, 1],
+      translateY: [-20, 0],
+      delay: anime.stagger(100),
+      ease: "inOutElastic"
+    });
+
+  document.getElementById("hamburger-toggle").addEventListener(
+    "click",
+    function(e) {
+      inTimeline.play();
+      if (inTimeline.began) {
+        inTimeline.reverse();
+      }
+    },
+    false
+  );
+})();
